03a49cb3483513ac8be6e65778ac86520cdd2664
sailorbunni <92497221+sailorbunni@users.noreply.github.com>
2021-11-18 18:37:51 -0600

    adding details

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,13 @@
-import React from "react";
-import axois from "axios";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 import ReactAnimatedWeather from "react-animated-weather";
 
 export default function Weather() {
+  const [city, setCity] = useState();
+  const [update, setUpdate] = useState({});
   const apiKey = "516d8557dd651a50157fe1f94e6882a3";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${weatherData.city}&appid=${apiKey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
   let weatherData = {
     city: "Tokyo",
     temperature: 18,
@@ -14,10 +16,25 @@ export default function Weather() {
     humidity: 42,
     wind: 6,
   };
+  function updateWeather(response) {
+    setUpdate({
+      temp: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    axios.get(apiUrl).then(updateWeather);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
 
   return (
     <div className="Weather">
-      <form className="mb-3">
+      <form onSubmit={handleSubmit} className="mb-3">
         <div className="row">
           <div className="col-9">
             <input
@@ -25,6 +42,7 @@ export default function Weather() {
               placeholder="Type a city.."
               className="form-control"
               autoComplete="off"
+              onChange={updateCity}
             />
           </div>
           <div className="col-3">
@@ -37,7 +55,7 @@ export default function Weather() {
         </div>
       </form>
       <div className="overview">
-        <h1>{weatherData.city}</h1>
+        <h1>{city}</h1>
         <ul>
           <li>Last updated: {weatherData.date}</li>
           <li>{weatherData.description}</li>
